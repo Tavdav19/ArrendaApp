6df57ab3a1d3628e60e1ad6a1d0134ab52c8be1f
Tavdav19 <35740230+Tavdav19@users.noreply.github.com>
2018-05-30 17:22:02 +0100

    Design Interface
    Novos layouts das activitys

--- app/src/main/java/com/example/tavar/arrendaapp/FeedActivity.java
+++ app/src/main/java/com/example/tavar/arrendaapp/FeedActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_feed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -25,5 +25,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
