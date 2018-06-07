d7c66ddc195cd21945eae3333a931b12b8670c1d
Tavdav19 <35740230+Tavdav19@users.noreply.github.com>
2018-06-07 16:28:03 +0100

    New java class House

---
+++ app/src/main/java/com/example/tavar/arrendaapp/House.java
@@ -0,0 +1,15 @@
+package com.example.tavar.arrendaapp;
+
+public class House {
+    private long id;
+    private String description;
+    private String Loc;
+    private long idLoc;
+    private String seller;
+    private long idSeller;
+    private int people;
+    private int bedroom;
+    private int Bathroom;
+
+
+}
