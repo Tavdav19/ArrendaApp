7a3c7379f1cfc8c6c3276c1c8e375e3d62118349
Tavdav19 <35740230+Tavdav19@users.noreply.github.com>
2018-06-07 16:33:24 +0100

    New java class DbTableHouse

---
+++ app/src/main/java/com/example/tavar/arrendaapp/DbTableHouse.java
@@ -0,0 +1,12 @@
+package com.example.tavar.arrendaapp;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class DbTableHouse implements BaseColumns {
+    private SQLiteDatabase db;
+
+    public DbTableHouse (SQLiteDatabase db){
+        this.db = db;
+    }
+}
