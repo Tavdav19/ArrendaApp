9475666b44206b615d57f603c0fed842c92f7419
Tavdav19 <35740230+Tavdav19@users.noreply.github.com>
2018-06-07 16:29:02 +0100

    get/set java class House

--- app/src/main/java/com/example/tavar/arrendaapp/House.java
+++ app/src/main/java/com/example/tavar/arrendaapp/House.java
@@ -11,5 +11,75 @@ public class House {
     private int bedroom;
     private int Bathroom;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLoc() {
+        return Loc;
+    }
+
+    public void setLoc(String loc) {
+        Loc = loc;
+    }
+
+    public long getIdLoc() {
+        return idLoc;
+    }
+
+    public void setIdLoc(long idLoc) {
+        this.idLoc = idLoc;
+    }
+
+    public String getSeller() {
+        return seller;
+    }
+
+    public void setSeller(String seller) {
+        this.seller = seller;
+    }
+
+    public long getIdSeller() {
+        return idSeller;
+    }
+
+    public void setIdSeller(long idSeller) {
+        this.idSeller = idSeller;
+    }
+
+    public int getPeople() {
+        return people;
+    }
+
+    public void setPeople(int people) {
+        this.people = people;
+    }
+
+    public int getBedroom() {
+        return bedroom;
+    }
+
+    public void setBedroom(int bedroom) {
+        this.bedroom = bedroom;
+    }
+
+    public int getBathroom() {
+        return Bathroom;
+    }
+
+    public void setBathroom(int bathroom) {
+        Bathroom = bathroom;
+    }
 }
