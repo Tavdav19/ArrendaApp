a32727f103fcff44707785cc636867e25504e001
Tavdav19 <35740230+Tavdav19@users.noreply.github.com>
2018-07-18 20:09:23 +0100

    Creation CursorAdapter

--- app/src/androidTest/java/com/example/tavar/arrendaapp/ArrendaDbTest.java
+++ app/src/androidTest/java/com/example/tavar/arrendaapp/ArrendaDbTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -15,12 +16,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class ArrendaDbTest {
+    @Before
+    public void setUp (){getContext().deleteDatabase(DbOpenHelper.NAME_DB);}
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.tavar.arrendaapp", appContext.getPackageName());
+    }
+    private Context getContext(){
+        return InstrumentationRegistry.getTargetContext();
     }
 }
